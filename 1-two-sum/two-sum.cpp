0b01ce8f61c46483050b77a1df2cc6e4a64cf0e5
akarshgoel333 <akarshgoel333@gmail.com>
2025-12-27 15:58:22 +0530

    Time: 1 ms (76.25%) | Memory: 15.1 MB (10.61%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -5,7 +5,7 @@ public:
         for(int i=0; i<nums.size(); i++){
             int ab = target - nums[i];
             if(ans.find(ab)!=ans.end()) return {i,ans[ab]};
-            ans[nums[i]]=i;
+            else ans[nums[i]]=i;
         }
         return {-1,-1};
     }
