0772d83e02d9905b7298d903b77c2c73bbc6044f
akarshgoel333 <akarshgoel333@gmail.com>
2026-01-12 10:31:21 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (8.23%) - LeetSync

--- 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
         int n = points.size();
-        int inx = points[0][0];
-        int iny = points[0][1];
         int ans = 0;
-        for(int i=1; i<n; i++){
-            int a = abs(inx-points[i][0]);
-            int b = abs(iny-points[i][1]);
-            inx = points[i][0];
-            iny = points[i][1];
+        for(int i=0; i<n-1; i++){
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            int x2 = points[i+1][0];
+            int y2 = points[i+1][1];
+            int a = abs(x1-x2);
+            int b = abs(y1-y2);
             ans += max(a,b);
             // cout<<ans<<" ";
         }
