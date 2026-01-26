7055ac32a103d45230d179b253494ee5ff0f9eeb
akarshgoel333 <akarshgoel333@gmail.com>
2026-01-26 13:07:11 +0530

    Time: 12 ms (89.78%) | Memory: 36.6 MB (88.73%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -4,12 +4,16 @@ public:
         int n = arr.size();
         int mini = INT_MAX;
         sort(arr.begin(),arr.end());
-        for(int i=0; i<n-1; i++){
-            mini = min(arr[i+1]-arr[i], mini);
-        }
         vector<vector<int>> ans;
         for(int i=0; i<n-1; i++){
-            if(arr[i+1]-arr[i] == mini) ans.push_back({arr[i],arr[i+1]});
+            if(arr[i+1]-arr[i]<mini){
+                mini = min(arr[i+1]-arr[i],mini);
+                ans.clear();
+                ans.push_back({arr[i],arr[i+1]});
+            }
+            else if(arr[i+1]-arr[i]==mini){
+                ans.push_back({arr[i],arr[i+1]});
+            }
         }
         return ans;
     }
