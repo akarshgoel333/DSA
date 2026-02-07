0da516c5d3d03fed3fa08e0526501842ea70201a
akarshgoel333 <akarshgoel333@gmail.com>
2026-02-07 20:38:02 +0530

    Time: 7 ms (97.78%) | Memory: 25.6 MB (54.88%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.length();
         int bba=0, del=0;
-        for(int i=0; i<n; i++){
-            if(s[i]=='b'){
+        for(auto cc: s){
+            if(cc == 'b'){
                 bba++;
             }
             else if(bba>0){
