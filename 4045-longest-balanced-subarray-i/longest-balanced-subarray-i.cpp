cda68ef84b7f254fa397092852478562d7ec095d
akarshgoel333 <akarshgoel333@gmail.com>
2026-02-10 13:18:36 +0530

    Time: 42 ms (98.32%) | Memory: 35 MB (100.00%) - LeetSync

--- 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -1,22 +1,20 @@
+constexpr int N=1e5+1;
+short freq[N]={0};
 class Solution {
 public:
-    int longestBalanced(vector<int>& nums) {
-        int n = nums.size();
-        int maxi = 0;
-        for(int i=0; i<n; i++){
-            set<int>cnt;
-            int odd=0, eve=0;
-            for(int j=i; j<n; j++){
-                if(cnt.find(nums[j]) == cnt.end()){
-                    if(nums[j]%2==1) odd++;
-                    else eve++;
-                }
-                cnt.insert(nums[j]);
-                if(odd==eve){
-                    maxi = max(maxi,j-i+1);
-                }
+    static int longestBalanced(vector<int>& nums) {
+        const int n=nums.size();
+        int len=0;
+        for(int l=0; l<n; l++){
+            int cnt[2]={0};
+            for(int r=l; r<n; r++){
+                const int x=nums[r];
+                if (++freq[x]==1) cnt[x&1]++;
+                if (cnt[0]==cnt[1])
+                    len=max(len, r-l+1);
             }
+            for(int i=l; i<n; i++) freq[nums[i]]=0;
         }
-        return maxi;
+        return len;
     }
 };
