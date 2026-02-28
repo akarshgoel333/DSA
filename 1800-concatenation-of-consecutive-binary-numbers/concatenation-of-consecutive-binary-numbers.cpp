5113508511d54ff80ad4db811fc1105f05587fee
akarshgoel333 <akarshgoel333@gmail.com>
2026-02-28 12:36:07 +0530

    Time: 21 ms (97.32%) | Memory: 8.2 MB (41.61%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -6,7 +6,7 @@ public:
         int len = 0;
         for(int i=1; i<=n; i++){
             if((i & (i-1)) == 0) len++;
-            ans = ((ans << len) % MOD + i) % MOD;
+            ans = ((ans << len) | i) % MOD;
         }
         return ans;
     }
