cc6e297b1f2457c2a6abce194b7ea74f8e5a0e19
akarshgoel333 <akarshgoel333@gmail.com>
2026-03-11 12:34:29 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (26.22%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -4,7 +4,7 @@ public:
         if(n==0) return 1;
         if(n==1) return 0;
         int m = log2(n)+1;
-        int a = pow(2,m) - 1;
+        int a = (1<<m) - 1;
         return a-n;
     }
 };
