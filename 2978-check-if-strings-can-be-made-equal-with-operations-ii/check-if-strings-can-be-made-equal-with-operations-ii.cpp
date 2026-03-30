e24b03b920a0c03ed832202673343428ecf34fac
akarshgoel333 <akarshgoel333@gmail.com>
2026-03-30 17:18:02 +0530

    Time: 12 ms (46.55%) | Memory: 19.4 MB (87.36%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -2,19 +2,6 @@ class Solution {
 public:
     bool checkStrings(string s1, string s2) {
         int n = s1.length();
-        int even = 0;
-        int odd = 0;
-        for(int i=0; i<n; i++){
-            if(i%2==0){
-                even += s1[i] - 'a';
-                even -= s2[i] - 'a';
-            }
-            else{
-                odd += s1[i] - 'a';
-                odd -= s2[i] - 'a';
-            }
-        }
-        if(even!=0 || odd!=0) return false;
         vector<int>eve(26,0);
         vector<int>oddd(26,0);
         for(int i=0; i<n; i++){
