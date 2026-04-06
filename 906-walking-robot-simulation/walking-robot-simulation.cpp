c6e7fe0f996e231010ca4778f1140d724aa42dbd
akarshgoel333 <akarshgoel333@gmail.com>
2026-04-07 04:12:45 +0530

    Time: 67 ms (23.06%) | Memory: 40.2 MB (31.10%) - LeetSync

---
+++ 906-walking-robot-simulation/walking-robot-simulation.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        unordered_set<string>obz;
+        for(auto &obs: obstacles){
+            string str = to_string(obs[0])+" "+to_string(obs[1]);
+            obz.insert(str);
+        }
+        int x = 0;
+        int y = 0;
+        int maxD = 0;
+        pair<int,int>dir = {0,1};
+        for(int i=0; i<commands.size(); i++){
+            if(commands[i]==-2){
+                dir = {-dir.second,dir.first};
+            }
+            else if(commands[i]==-1){
+                dir = {dir.second,-dir.first};
+            }
+            else{
+                for(int step=0; step<commands[i]; step++){
+                    int newx = x+dir.first;
+                    int newy = y+dir.second;
+                    string nxtstep = to_string(newx)+" "+to_string(newy);
+                    if(obz.find(nxtstep) != obz.end()){
+                        break;
+                    }
+                    x=newx;
+                    y=newy;
+                }
+            }
+            maxD = max(maxD, x*x + y*y);
+        }
+        return maxD;
+    }
+};
