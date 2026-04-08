3d7b450e40187f2d86590eefc8cd05306fd935f6
akarshgoel333 <akarshgoel333@gmail.com>
2026-04-09 00:49:00 +0530

    Time: 64 ms (55.50%) | Memory: 157.9 MB (91.87%) - LeetSync

--- 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
+++ 3974-xor-after-range-multiplication-queries-i/xor-after-range-multiplication-queries-i.cpp
@@ -3,10 +3,9 @@ public:
     int xorAfterQueries(vector<int>& nums, vector<vector<int>>& q) {
         const int mod = 1e9+7;
         for(auto &x: q){
-            int idx = x[0];
-            while(idx<=x[1]){
-                nums[idx] = (1LL*(nums[idx]%mod)*(x[3]%mod))%mod;
-                idx += x[2];
+            int l = x[0], r = x[1], k = x[2], v = x[3];
+            for(int idx=l; idx<=r; idx+=k){
+                nums[idx] = (1LL*nums[idx]*x[3])%mod;
             }
         }
         int ans = 0;
