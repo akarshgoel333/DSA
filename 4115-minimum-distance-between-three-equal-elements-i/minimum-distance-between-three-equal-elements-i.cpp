da40bd87f27d45890ff5a34251bd8332f158576b
akarshgoel333 <akarshgoel333@gmail.com>
2026-04-10 11:44:02 +0530

    Time: 3 ms (71.85%) | Memory: 28.6 MB (90.97%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.cpp
@@ -4,8 +4,8 @@ public:
         int n = nums.size();
         bool flag=false;
         int ans = INT_MAX;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
+        for(int i=0; i<n-2; i++){
+            for(int j=i+1; j<n-1; j++){
                 for(int k=j+1; k<n; k++){
                     if(nums[i]==nums[j] && nums[i]==nums[k]){
                         flag=true;
