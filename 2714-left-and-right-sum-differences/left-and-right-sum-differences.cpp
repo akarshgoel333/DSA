2c98ba15bfc74c6a0777e19e5196bcbc2d977194
akarshgoel333 <akarshgoel333@gmail.com>
2026-06-06 23:08:06 +0530

    Time: 0 ms (100.00%) | Memory: 14.9 MB (81.37%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.cpp
@@ -3,10 +3,12 @@ public:
     vector<int> leftRightDifference(vector<int>& nums) {
         int n = nums.size();
         vector<int>arr(n,0);
+        int sum = nums[0];
         for(int i=1; i<n; i++){
-            arr[i] += nums[i-1]+arr[i-1];
+            arr[i] += sum;
+            sum += nums[i];
         }
-        int sum = 0;
+        sum = 0;
         for(int i=n-2; i>=0; i--){
             sum += nums[i+1];
             arr[i] = abs(arr[i]-sum);
