36bc1f05d38488813e32daa36da716aff03d112a
akarshgoel333 <akarshgoel333@gmail.com>
2026-07-12 12:54:15 +0530

    Time: 75 ms (16.81%) | Memory: 43.4 MB (75.05%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
         int n = arr.size();
+        vector<int>ans = arr;
+        sort(ans.begin(),ans.end());
         map<int,int> mp;
-        for(auto &x: arr){
-            mp[x] = 1;
-        }
         int var = 1;
-        for(auto &[key,val]: mp){
-            val = var;
-            var++;
+        for(auto &x: ans){
+            if(mp.find(x)==mp.end()){
+                mp[x] = var;
+                var++;
+            }
         }
-        vector<int>ans(n);
         for(int i=0; i<n; i++){
             ans[i] = mp[arr[i]];
         }
