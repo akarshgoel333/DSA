fd3bf9077fda5e6f78b9890acd9c6c91aa48d407
akarshgoel333 <akarshgoel333@gmail.com>
2026-08-25 16:12:02 +0530

    Time: 3 ms (38.06%) | Memory: 24 MB (94.59%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        set<int>s(nums.begin(),nums.end());
+        vector<bool>s(101,0);
+        for(auto n: nums) s[n]=1;
         int i=k;
         for(i=k; i<101; i=i+k){
-            if(!s.count(i)) return i;
+            if(!s[i]) return i;
         }
         return i;
     }
