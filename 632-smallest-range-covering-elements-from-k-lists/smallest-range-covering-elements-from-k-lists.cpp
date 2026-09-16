dc36077161e32e864d7c0a2ded63b2688feb8f22
akarshgoel333 <akarshgoel333@gmail.com>
2026-09-17 01:13:27 +0530

    Time: 79 ms (61.50%) | Memory: 36.6 MB (88.80%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        vector<pair<int,pair<int,int>>>arr;
+
+        priority_queue< pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>> >pq;
         int maxi = INT_MIN;
         for(int i=0; i<nums.size(); i++){
-            arr.push_back(make_pair(nums[i][0],make_pair(i,0)));
+            pq.push(make_pair(nums[i][0],make_pair(i,0)));
             maxi = max(maxi,nums[i][0]);
         }
-
-        priority_queue< pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>> >pq(arr.begin(),arr.end());
         int mini = pq.top().first;
         vector<int>ans = {mini,maxi};
         pair<int,pair<int,int>> res;
