6d7fecd50e4d359956eae59779a9e395ab774e20
akarshgoel333 <akarshgoel333@gmail.com>
2026-09-21 15:47:07 +0530

    Time: 138 ms (56.25%) | Memory: 258.8 MB (81.25%) - LeetSync

---
+++ 4417-number-of-intersecting-interval-pairs-ii/number-of-intersecting-interval-pairs-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long countIntersectingIntervals(vector<vector<int>>& ivl) {
+        int n = ivl.size();
+        priority_queue<int, vector<int>, greater<>> pq;
+        sort(ivl.begin(),ivl.end());
+        long long cnt = 0;
+        pq.push(ivl[0][1]);
+        for(int i=1; i<n; i++){
+            while(!pq.empty() && pq.top()<ivl[i][0]){
+                pq.pop();
+            }
+            cnt += pq.size();
+            pq.push(ivl[i][1]);
+        }
+        return cnt;
+    }
+};
